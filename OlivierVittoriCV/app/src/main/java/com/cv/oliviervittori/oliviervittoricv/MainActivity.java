2238cfa5903c9e85707233f2bd44895eeb80c42d
vittoGit <olivier.vittori@gmail.com>
2015-09-30 14:01:51 +0200

    test

--- OlivierVittoriCV/app/src/main/java/com/cv/oliviervittori/oliviervittoricv/MainActivity.java
+++ OlivierVittoriCV/app/src/main/java/com/cv/oliviervittori/oliviervittoricv/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // test
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
